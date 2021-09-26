1b9583617d2171ac8c215305858faf91cb02b033
Asish <mohapatra.asish@gmail.com>
2021-09-27 01:11:48 +0800

    Iterate 2 list in one foreach

---
+++ IterateTwoList/IterateTwoList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IterateTwoList
+{
+    class Program
+    {
+        static List<int> ids = new List<int>(new[] { 1, 2, 3, 4 });
+        static List<string> names = new List<string>(new[] { "A", "B", "C", "D" });
+        public static void Main(string[] args)
+        {
+            var result = ids.Zip(names, (x, y) => new { Id = x, Name = y });
+            result.ToList().ForEach(x => Console.WriteLine($"Id = {x.Id} and Name  {x.Name}"));
+        }
+    }
+}
